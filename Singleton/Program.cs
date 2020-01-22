e9dc7ddd10d792ffa2056f970cf5c042aed597b5
Dmitriy Morozov <dmitriy_morozov_1997@mail.ru>
2020-01-22 21:58:47 +0300

    Implement Singleton pattern.

---
+++ Singleton/Program.cs
@@ -0,0 +1,25 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+// 
+
+using System;
+using System.Threading;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Thread(() =>
+            {
+                Singleton singleton1 = Singleton.Instance;
+                Console.WriteLine(singleton1.Date);
+            })).Start();
+
+            Singleton singleton2 = Singleton.Instance;
+            Console.WriteLine(singleton2.Date);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,24 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+// 
+
+using System;
+
+namespace Singleton
+{
+    internal sealed class Singleton
+    {
+        static Singleton()
+        {
+        }
+
+        private Singleton()
+        {
+            Date = DateTime.UtcNow.TimeOfDay.ToString();
+        }
+
+        public string Date { get; }
+
+        public static Singleton Instance { get; } = new Singleton();
+    }
+}
